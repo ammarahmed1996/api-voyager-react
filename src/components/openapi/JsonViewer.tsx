ef373ce6cc195127527c1019a7cb34887e533f4c
Ammar <ammar.ahmed.it@gmail.com>
2025-05-24 19:08:26 +0200

    a

--- src/components/openapi/JsonViewer.tsx
+++ src/components/openapi/JsonViewer.tsx
@@ -10,12 +10,17 @@ interface JsonViewerProps {
 
 const JsonViewer: React.FC<JsonViewerProps> = ({ json, title }) => {
   const dataString = typeof json === 'string' ? json : JSON.stringify(json, null, 2);
-
+    const customTheme = {
+        ...okaidia,
+        'property': { color: '#0265D2' }, // Postman-like orange
+        // Values#40b4d4
+        'string': { color: '#FF6C37' },   // Postman-like blue
+    };
   return (
     <div className="my-4">
       {title && <h4 className="text-md font-semibold mb-2 text-foreground">{title}</h4>}
       <div className="bg-muted p-4 rounded-md max-h-96 overflow-auto">
-        <SyntaxHighlighter language="json" style={okaidia} customStyle={{ margin: 0, background: 'transparent' }}>
+        <SyntaxHighlighter language="json"  style={customTheme}>
           {dataString}
         </SyntaxHighlighter>
       </div>
